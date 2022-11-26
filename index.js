c7c10cc096f69ffddfd7957bb2f8aafcd70605db
sufian <sufian4se@gmail.com>
2022-11-26 13:59:07 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-const express = require('express');
-const cors = require('cors');
-const port  = process.env.PORT || 5000;
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', async(req, res) => {
-  res.send('resale market server is running')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ilq1t0j.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try{
+
+  } finally {
+    
+  }
+
+}
+run().catch(console.log())
+
+app.get("/", async (req, res) => {
+  res.send("resale market server is running");
+});
 
 app.listen(port, () => {
-  console.log(`Resale market is running on ${port}`)
-})
+  console.log(`Resale market is running on ${port}`);
+});
