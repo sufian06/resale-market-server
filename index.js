91917936465cd05226490b3446c71606bab17ed2
sufian <sufian4se@gmail.com>
2022-11-26 15:07:53 +0600

    category id wise route added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -11,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ilq1t0j.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -24,6 +23,13 @@ async function run() {
 
     app.get('/category', async(req, res) => {
       const query = {};
+      const category = await productsCollections.find(query).toArray();
+      res.send(category)
+    });
+
+    app.get('/category/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
       const category = await productsCollections.find(query).toArray();
       res.send(category)
     })
