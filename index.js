d4fd7945ea7291ceed821089a33294ef3b1a00e8
sufian <sufian4se@gmail.com>
2022-11-27 18:09:19 +0600

    bookings api created

--- index.js
+++ index.js
@@ -56,6 +56,14 @@ async function run() {
       res.send(result);
     });
 
+    // booking api
+    app.get('/bookings', async(req, res) => {
+      const email = req.query.email;
+      const query = {email: email};
+      const bookings = await bookingsCollection.find(query).toArray();
+      res.send(bookings);
+    })
+
     app.post('/bookings', async(req, res) => {
       const booking = req.body;
       const result = await bookingsCollection.insertOne(booking)
